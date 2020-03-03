642a649d5ad80dc691a8d545a17d00ea4d04523f
Ankur rai <rai.ankur0007@gmail.com>
2020-03-03 23:16:53 +0530

    reduced test cases to 5 from 10

--- vowel_check.c
+++ vowel_check.c
@@ -8,18 +8,13 @@ int main()
     printf("Input a Character :  ");
     scanf("%c", &ch);
 
-    switch(ch)
+    switch(tolower(ch))
     {
         case 'a':
-        case 'A':
         case 'e':
-        case 'E':
         case 'i':
-        case 'I':
         case 'o':
-        case 'O':
         case 'u':
-        case 'U':
             
             printf("\n\n%c is a vowel.\n\n", ch);
             break;
